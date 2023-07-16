28e6fc34ef2fac10453220b2c54ec7feb0dde0e4
Pac12Run8000 <gangstarr78992@>
2023-07-16 16:40:03 -0700

    Initial Commit

---
+++ StateObjectExample/StateObjectExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateObjectExampleApp.swift
+//  StateObjectExample
+//
+//  Created by Michelle Grover on 7/16/23.
+//
+
+import SwiftUI
+
+@main
+struct StateObjectExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
