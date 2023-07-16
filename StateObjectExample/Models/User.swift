ede92adc63e5465e7e07d6f61618ea611bfb796e
Pac12Run8000 <gangstarr78992@>
2023-07-16 16:43:56 -0700

    Added custom object

---
+++ StateObjectExample/Models/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  StateObjectExample
+//
+//  Created by Michelle Grover on 7/16/23.
+//
+
+import SwiftUI
+
+class User: ObservableObject {
+    // Publish any changes to the username property
+    @Published var username: String
+    @Published var age: Int
+
+    init(username: String, age:Int) {
+        self.username = username
+        self.age = age
+    }
+}
